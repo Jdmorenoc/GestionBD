a207f25b7ffd7b76742731172958c0424328b387
Juan Diego Moreno <Jdmorenoc@ufpso.edu.co>
2026-03-03 18:34:19 -0600

    update y delete explicado

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ app.get("/save", async(req, res)=> {
         'info:03578',
         JSON.stringify(json),
         {
-            EX: 60
+            EX: 300
         }
 
     )
@@ -32,6 +32,47 @@ app.get("/get", async (req, res)=> {
 
 })
 
+
+app.get("/update", async(req, res) => {
+    const edad = 32;
+    const data = await redis.get('info:03578');
+    if(!data){
+        return res.json({'success': false, 'data':[], 'msg': 'Not found'}, 404)
+    }
+    let json = JSON.parse(data);
+    json.edad = edad;
+    const response = await redis.set('info:03578',JSON.stringify(json) , {
+        EX : 300
+    });
+    const r = await redis.get('info:03578');
+    res.json({'success': response === 'OK', data:r, msg:response },200)
+});
+
+
+app.get('/hset', async(req,res)=>{
+    const response = await redis.hSet('info:192230', {
+        'name': 'Juan',
+        'apellido': 'Moreno',
+        'age': 22
+    });
+    await redis.expire('info:192230', 300)
+    res.send(response)
+})
+
+app.get('/getHash' , async(req, res) => {
+    const response = await redis.hGetAll('info:192230');
+    const ttl = await redis.ttl('info:192230')
+    res.json({success: true , data : response, ttl})
+})
+
+app.get("/delete", async (req,res) => {
+//const data = await redis.hDel('info:192230')
+const data = await redis.hDel('info:192230', 'age') 
+const data2 = await redis.hGetAll('info:192230')
+    const response = await redis.hGetAll('info:192230')
+res.send(response)
+
+})
 
 app.listen(8000, ()=> {
     console.log("Hello");
